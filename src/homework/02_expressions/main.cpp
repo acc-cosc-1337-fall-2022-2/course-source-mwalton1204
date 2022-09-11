b3f2ca2af798e8531a648c07ae3128198253f963
Michael Walton <michael.walton@g.austincc.edu>
2022-09-11 05:27:48 +0000

    HW2 - Added Comments

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -9,21 +9,30 @@ using std::cout;
 //main function
 int main()
 {
-	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+	//Declare variables
+	double meal_amount, 
+	tip_rate, tip_amount, tax_amount, total;
 
+	//Get cost of meal
 	cout<<"Enter the cost of the meal: ";
 	cin>>meal_amount;
 
+	//Calculate sales tax
 	tax_amount = get_sales_tax_amount(meal_amount);
 
+	//Get tip percentage and convert to decimal
 	cout<<"\nEnter the percentage that you would like to tip: ";
 	cin>>tip_rate;
-	tip_rate = tip_rate/100; //convert tip rate from integer to decimal
 
+	tip_rate = tip_rate/100;
+
+	//Calculate tip amount
 	tip_amount = get_tip_amount(meal_amount, tip_rate);
 
+	//Calculate total cost
 	total = meal_amount + tax_amount + tip_amount;
 
+	//Display itemized costs and total
 	cout<<"\nMeal Amount:   $"<<meal_amount;
 	cout<<"\nSales Tax:     $"<<tax_amount;
 	cout<<"\nTip Amount:    $"<<tip_amount;
