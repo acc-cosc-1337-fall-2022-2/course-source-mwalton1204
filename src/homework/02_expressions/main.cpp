a3f366cc5ba12f0f9296c3ecf623da8382e61c41
Michael Walton <michael.walton@g.austincc.edu>
2022-09-11 05:14:52 +0000

    HW2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,33 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
+using std::cin;
+using std::cout;
 
-
-/*
-Call multiply_numbers with 10 and 10 parameter values and display function result
-*/
+//main function
 int main()
 {
-	
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	cout<<"Enter the cost of the meal: ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"\nEnter the percentage that you would like to tip: ";
+	cin>>tip_rate;
+	tip_rate = tip_rate/100; // convert from percentage to decimal
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tax_amount + tip_amount;
+
+	cout<<"\nMeal Amount:   "<<meal_amount;
+	cout<<"\nSales Tax:     "<<tax_amount;
+	cout<<"\nTip Amount:    "<<tip_amount;
+	cout<<"\nTotal:         "<<total<<"\n\n";
 
 	return 0;
 }
